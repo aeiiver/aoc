1d574085c03d9060ae23fdcd6a354bd247a87011
aeiiver <101661283+aeiiver@users.noreply.github.com>
2022-12-02 12:59:32 +0100

    2022.02: first time trying out Rust went bad

---
+++ 2022/02/src/day2.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let game_plan = include_bytes!("./day2.prod");
+
+    // 15572
+    println!(
+        "{:?}",
+        game_plan
+            .split(|&byte| byte == b'\n')
+            .map(|round| {
+                let abc = (round[0] - b'A') as i8;
+                let xyz = (round[2] - b'X') as i8;
+                // win_score                                     shape_score
+                ([3, 0, 6][(abc - xyz).rem_euclid(3) as usize] + xyz + 1) as u32
+            })
+            .sum::<u32>()
+    );
+
+    // 16098
+    println!(
+        "{:?}",
+        game_plan
+            .split(|&b| b == b'\n')
+            .map(|round| {
+                let abc = round[0] - b'A';
+                let xyz = round[2] - b'X';
+                // (abc + offset) = shape_score                      win_score
+                ((abc + [2, 0, 1][xyz as usize]).rem_euclid(3) + 1 + [0, 3, 6][xyz as usize]) as u32
+            })
+            .sum::<u32>()
+    );
+}
