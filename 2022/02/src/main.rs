7138c3b8d0d1796fd24da9cfb8e5e3083cb7282a
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-07-07 06:55:13 +0200

    2022.02: actually save input files as text file

--- 2022/02/src/main.rs
+++ 2022/02/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let input = include_bytes!("./day02-prod");
+    let input = include_bytes!("./input-prod");
 
     // 15572
     println!(
         "{:?}",
         input
             .split(|&byte| byte == b'\n')
+            .take_while(|round| !round.is_empty())
             .map(|round| {
                 let abc = round[0] - b'A';
                 let xyz = round[2] - b'X';
@@ -20,13 +21,14 @@ fn main() {
     println!(
         "{:?}",
         input
-            .split(|&b| b == b'\n')
+            .split(|&byte| byte == b'\n')
+            .take_while(|round| !round.is_empty())
             .map(|round| {
                 let abc = round[0] - b'A';
                 let xyz = round[2] - b'X';
-                let win_score = [0, 3, 6][xyz as usize];
-                let shape_score = (abc + [2, 0, 1][xyz as usize]).rem_euclid(3) + 1;
-                (win_score + shape_score) as u32
+                let win_score = [0, 3, 6][xyz as usize] as u32;
+                let shape_score = ((abc + [2, 0, 1][xyz as usize]).rem_euclid(3) + 1) as u32;
+                win_score + shape_score
             })
             .sum::<u32>()
     );
