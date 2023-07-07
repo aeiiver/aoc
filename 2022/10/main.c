384a46febc6ee91745b8cfa88a2fa9601df0c8cd
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-07-07 16:53:14 +0200

    2022.10: C was the winner of the game all along

--- 2022/10/main.c
+++ 2022/10/main.c
@@ -5,9 +5,13 @@
 #define INST_DELIM " "
 
 int main() {
-  FILE *problem_input = fopen("day10.prod", "r");
+  FILE *input = fopen("./input-prod", "r");
+  if (input == NULL) {
+    perror("fopen");
+    exit(1);
+  }
 
-  int acc = 1;
+  int accumul = 1;
   int cycle_num = 0;
 
   // partone
@@ -18,7 +22,7 @@ int main() {
   char *token;
   int opcycles;
   int is_addx;
-  for (char line[101]; fgets(line, sizeof(line), problem_input) != NULL;) {
+  for (char line[101]; fgets(line, sizeof(line), input) != NULL;) {
     is_addx = 0;
 
     // first token is an instruction
@@ -30,8 +34,8 @@ int main() {
       opcycles = 2;
       is_addx = 1;
     } else {
-      printf("Something is wrong...");
-      fclose(problem_input);
+      printf("Input is wrong somehow... Gotta bet I trolled a little it");
+      fclose(input);
       exit(1);
     }
 
@@ -40,36 +44,37 @@ int main() {
 
       /* partone */
       if (20 <= cycle_num && cycle_num <= 220 && (cycle_num + 20) % 40 == 0) {
-        sig_strength += acc * cycle_num;
+        sig_strength += accumul * cycle_num;
       }
 
-      // DEBUG
-      // printf("[%d]: %d ~ %d, %d\n", cycle_num, acc, sig_strength,
-      //        acc * cycle_num);
+      /* DEBUG block */
+      // printf("[%d]: %d ~ %d, %d\n", cycle_num, accumul, sig_strength,
+      //        accumul * cycle_num);
 
       /* parttwo */
       if (0 <= cycle_num && cycle_num <= 240) {
         // position of the crt cursor: (cycle_num - 1) % 40
         // position of the sprite: acc
 
-        if (abs((cycle_num - 1) % 40 - acc) < 2) {
+        if (abs((cycle_num - 1) % 40 - accumul) < 2) {
           strcat(crt_display, "#");
         } else {
           strcat(crt_display, ".");
         }
-        if ((cycle_num - 1) % 40 == 39)
+        if ((cycle_num - 1) % 40 == 39) {
           strcat(crt_display, "\n");
+        }
       }
     }
 
     if (is_addx) {
       // second token is an operand
       token = strtok(NULL, INST_DELIM);
-      acc += atoi(token);
+      accumul += atoi(token);
     }
   }
 
-  fclose(problem_input);
+  fclose(input);
 
   // partone: 16880
   printf("Signal strength: %d\n", sig_strength);
@@ -78,14 +83,12 @@ int main() {
   printf("CRT display:\n%s", crt_display);
 
   /*
-
-  ###..#..#..##..####..##....##.###..###..
-  #..#.#.#..#..#....#.#..#....#.#..#.#..#.
-  #..#.##...#..#...#..#..#....#.###..#..#.
-  ###..#.#..####..#...####....#.#..#.###..
-  #.#..#.#..#..#.#....#..#.#..#.#..#.#.#..
-  #..#.#..#.#..#.####.#..#..##..###..#..#.
-
+     ###..#..#..##..####..##....##.###..###..
+     #..#.#.#..#..#....#.#..#....#.#..#.#..#.
+     #..#.##...#..#...#..#..#....#.###..#..#.
+     ###..#.#..####..#...####....#.#..#.###..
+     #.#..#.#..#..#.#....#..#.#..#.#..#.#.#..
+     #..#.#..#.#..#.####.#..#..##..###..#..#.
   */
 
   return 0;
